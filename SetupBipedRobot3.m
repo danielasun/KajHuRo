11b7728ce476883154e655e422e3a39b21417ea1
Daniel Sun <Daniel Sun>
2017-04-06 00:11:28 -0700

    edit SimulatePreviewDynamics to have y = C*q(2:4) modify SetupBipedRobot3 to use more constants edit dan_test_walking to have connected path

--- SetupBipedRobot3.m
+++ SetupBipedRobot3.m
@@ -1,4 +1,4 @@
-%%% SetupBipedRobot2.m
+%%% SetupBipedRobot3.m
 %%% Altering some of Kajita's original stuff, lengthening the legs and
 %%% using inverse kinematics to set up the original resting pose.
 global uLINK
@@ -9,20 +9,25 @@ UX = [1 0 0]';
 UY = [0 1 0]';
 UZ = [0 0 1]';
 
-uLINK    = struct('name','BODY'    , 'm', 10, 'sister', 0, 'child', 2, 'b',[0  0   0.7]','a',UZ,'q',0);
+linkLength = .4;
+comHeight = .7;
+GND = 0.109115348192675
+
+
+uLINK    = struct('name','BODY'    , 'm', 10, 'sister', 0, 'child', 2, 'b',[0  0   comHeight]','a',UZ,'q',0);
 
 uLINK(2) = struct('name','RLEG_J0' , 'm',  5, 'sister', 8, 'child', 3, 'b',[0 -0.1 0]'   ,'a',UZ,'q',0);
 uLINK(3) = struct('name','RLEG_J1' , 'm',  1, 'sister', 0, 'child', 4, 'b',[0  0   0]','a',UX,'q',0);
 uLINK(4) = struct('name','RLEG_J2' , 'm',  5, 'sister', 0, 'child', 5, 'b',[0  0   0]'   ,'a',UY,'q',0);
-uLINK(5) = struct('name','RLEG_J3' , 'm',  1, 'sister', 0, 'child', 6, 'b',[0  0  -0.4]' ,'a',UY,'q',0);
-uLINK(6) = struct('name','RLEG_J4' , 'm',  6, 'sister', 0, 'child', 7, 'b',[0  0  -0.4]' ,'a',UY,'q',0);
+uLINK(5) = struct('name','RLEG_J3' , 'm',  1, 'sister', 0, 'child', 6, 'b',[0  0  -linkLength]' ,'a',UY,'q',0);
+uLINK(6) = struct('name','RLEG_J4' , 'm',  6, 'sister', 0, 'child', 7, 'b',[0  0  -linkLength]' ,'a',UY,'q',0);
 uLINK(7) = struct('name','RLEG_J5' , 'm',  2, 'sister', 0, 'child', 0, 'b',[0  0   0  ]' ,'a',UX,'q',0);
 
 uLINK(8) = struct('name','LLEG_J0' , 'm',  5, 'sister', 0, 'child', 9, 'b',[0  0.1 0]'   ,'a',UZ,'q',0);
 uLINK(9) = struct('name','LLEG_J1' , 'm',  1, 'sister', 0, 'child',10, 'b',[0  0   0]','a',UX,'q',0);
 uLINK(10)= struct('name','LLEG_J2' , 'm',  5, 'sister', 0, 'child',11, 'b',[0  0   0]'   ,'a',UY,'q',0);
-uLINK(11)= struct('name','LLEG_J3' , 'm',  1, 'sister', 0, 'child',12, 'b',[0  0  -0.4]' ,'a',UY,'q',0);
-uLINK(12)= struct('name','LLEG_J4' , 'm',  6, 'sister', 0, 'child',13, 'b',[0  0  -0.4]' ,'a',UY,'q',0);
+uLINK(11)= struct('name','LLEG_J3' , 'm',  1, 'sister', 0, 'child',12, 'b',[0  0  -linkLength]' ,'a',UY,'q',0);
+uLINK(12)= struct('name','LLEG_J4' , 'm',  6, 'sister', 0, 'child',13, 'b',[0  0  -linkLength]' ,'a',UY,'q',0);
 uLINK(13)= struct('name','LLEG_J5' , 'm',  2, 'sister', 0, 'child', 0, 'b',[0  0   0  ]' ,'a',UX,'q',0);
 
 FindMother(1);   %?@–…?C–ºƒŠƒ“ƒN‚Ì?î•ñ‚ð‚à‚Æ‚É•êƒŠƒ“ƒN‚ð?Ý’è‚·‚é
@@ -65,17 +70,20 @@ uLINK(LLEG_J2).q = -10.0*ToRad;
 uLINK(LLEG_J3).q = 20.0*ToRad;
 uLINK(LLEG_J4).q = -10.0*ToRad;
 
-uLINK(BODY).p = [0.0, -.1, .7]';
+uLINK(BODY).p = [0.0, 0, comHeight]';
 uLINK(BODY).R = eye(3);
 
-Rfoot.p = [0,-.1,0]';
+ForwardKinematics(1);
+
+% Added by Daniel
+Rfoot.p = [0,-.1, GND]';
 Rfoot.R = RPY2R([0,0,0]);  %  -pi/4 < q < pi/4
-qR2 = IK_leg(uLINK(BODY),-0.1,0.4,0.4,Rfoot);
+qR2 = IK_leg(uLINK(BODY),-0.1,linkLength,linkLength,Rfoot);
 
 % set left foot
-Lfoot.p = [0,.1,0]';
+Lfoot.p = [0,.1, GND]';
 Lfoot.R = RPY2R([0,0,0]);  %  -pi/4 < q < pi/4
-qR3 = IK_leg(uLINK(BODY),0.1,0.4,0.4,Lfoot);
+qR3 = IK_leg(uLINK(BODY),0.1,linkLength,linkLength,Lfoot);
 for n=0:5
     uLINK(RLEG_J0+n).q = qR2(n+1);
     uLINK(LLEG_J0+n).q = qR3(n+1);
--- SimulatePreviewDynamics.m
+++ SimulatePreviewDynamics.m
@@ -1,6 +1,6 @@
-function [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,nTimeSteps)
+function [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,previewLength)
 %SIMULATE_PREVIEW_DYNAMICS
-%  [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,nTimeSteps)
+%  [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,previewLength)
 %
 %   INPUTS:
 %   sys = original system
@@ -11,11 +11,11 @@ function [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,nTi
 %   x0 = initial condition. 
 %   err0 = initial amount of error in y to start out with. If you're starting
 %   out, a good guess for this is usually zero.
-%   nTimeSteps = length of preview
+%   previewLength = number of time steps in preview
 %   
 %   OUTPUTS:
 %   q = vector of states for each time step
-%   y = vector of outputs for each time step
+%   y = vector of outputs for each time step for original system (not yt)
 %   u = vector of control inputs for each time step
 
     At = sys_t.a;
@@ -30,11 +30,11 @@ function [ q,y,u ] = SimulatePreviewDynamics(sys,sys_t,Gi,Gx,Gd,yref,x0,err0,nTi
     q = [err0, x0']';
     G = [Gi Gx];
     u = [];
-    y = Ct*q;
+    y = C*x0; % changed to C instead of Ct
     It = [eye(p);zeros(size(Bt,1)-p,size(Bt,2))];
 
-    for i=1:length(yref)-nTimeSteps-1
-        u(end+1) = -G*q(:,i) +  Gd*yref(i:i+nTimeSteps-1)';
+    for i=1:length(yref)-previewLength-1
+        u(end+1) = -G*q(:,i) +  Gd*yref(i:i+previewLength-1)';
         q(:,end+1) = At*q(:,i) + Bt*u(:,i) - It*yref(i+1);
         y(:,end+1) = C*q(p+1:end,i);
     end
